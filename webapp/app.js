90fe19e8c22ba52b0a9bcd05d645085684b365f9
ben <isieldor@gmail.com>
2020-03-04 01:40:45 -0600

    skeleton for front end built

--- webapp/app.js
+++ webapp/app.js
@@ -1,6 +1,43 @@
 let app = new Vue({
     el: '#app',
     data: {
-        message: "Hello World!"
+        message: "Hello World!",
+        app1Visable:true
+    },
+    methods: {
+        selectConstraints: function(){
+            constraintComponent.constraintsVisable = true;
+            app.app1Visable = false;
+        },
+        selectSchedule: function(){
+            scheduleComponent.scheduleVisable = true;
+            app.app1Visable = false;
+        },
     }
 })
+
+let constraintComponent = new Vue({
+    el: '#constraints',
+    data: {
+        constraintsVisable: false
+    }
+})
+
+let scheduleComponent = new Vue({
+    el: '#schedule',
+    data: {
+        scheduleVisable: false
+    }
+})
+
+
+/*
+NOTES
+
+okaaay... so that's the extremely simple stuff put together. At the moment,
+especially considering exactly what we're trying to build, I don't think a 
+single page-app is outside the realm of possibility. 
+
+Considering using a v-for (vue directive runs on each thing in a collection) to
+render constraints in their own components. Will need to see how that works.
+*/
