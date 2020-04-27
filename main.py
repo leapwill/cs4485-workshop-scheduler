2fbdbd17b137524c94fd73e08e27653579b7c9f7
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2020-04-27 12:37:16 -0500

    Opens page in new browser tab

--- main.py
+++ main.py
@@ -3,13 +3,17 @@
 from http_helpers import HTTP_Protocol
 import asyncio as aio
 import sys
+import webbrowser
 
 if sys.version_info < (3, 7):
     print('Please update to Python 3.7 or newer.')
     exit(37)
 
+# Open the page in the browser
+async def open_browser (hostname, port):
+    webbrowser.open_new_tab(f'http://{hostname}:{port}')
 
-async def main():
+async def main ():
     # Create the backend server object using a custom HTTP handler
     hostname = '127.0.0.1'
     port = 8080
@@ -17,13 +21,15 @@ async def main():
     loop = aio.get_running_loop()
     server = await loop.create_server(lambda: HTTP_Protocol(webapp_root),
                                       hostname, port, start_serving=False)
-    # TODO: open system's default browser?
+    start_server = aio.create_task(server.serve_forever())
+    start_browser = aio.create_task(open_browser(hostname, port))
+
     print(f'Open the website at http://{hostname}:{port}')
 
     # Begin accepting connections
-    # `async with` block automatically handles cleanup on close
-    async with server:
-        await server.serve_forever()
+    await start_server
+    # Open the page
+    await start_browser
 
 if __name__ == '__main__':
     if sys.version_info >= (3, 7):
